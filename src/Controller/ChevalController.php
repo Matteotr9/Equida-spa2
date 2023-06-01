954c7d3d8e489ace2fc969b9ccc27776da2bb96c
Matteotr9 <89576532+Matteotr9@users.noreply.github.com>
2023-06-01 15:36:50 +0200

    up

--- src/Controller/ChevalController.php
+++ src/Controller/ChevalController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\RaceDeCheval;
 use App\Entity\Client;
 use App\Entity\Cheval;
+use App\Entity\Vente;
 use App\Form\ChevalType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,8 +23,7 @@ use Symfony\Component\Serializer\Normalizer\AbstractItemNormalizer ;
 class ChevalController extends AbstractController
 {
 
-    #[Route('/cheval/nouveau', name:'app_cheval_nouveau')]
-
+    #[Route('/cheval/nouveau', name: 'app_cheval_nouveau')]
     public function nouveau(Request $request, ManagerRegistry $doctrine): Response
     {
 
@@ -33,13 +34,13 @@ class ChevalController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             //En attendant la connexion client
-            $client= $doctrine->getRepository(Client::class)->find(1);
+            $client = $doctrine->getRepository(Client::class)->find(1);
             $cheval->setClient($client);
 
             //$entityManager = $this->getDoctrine()->getManager();
             $entityManager = $doctrine->getManager();
             $entityManager->persist($cheval);
-            
+
             $entityManager->flush();
 
             $this->addFlash('success', 'Le cheval a été enregistré avec succès.');
@@ -54,27 +55,24 @@ class ChevalController extends AbstractController
     }
 
 
-    #[Route('/cheval/consulter/{idCheval}', name:'app_cheval_consulter')]
+    #[Route('/cheval/consulter/{idCheval}', name: 'app_cheval_consulter')]
     public function consulterCheval($idCheval, Request $request, ManagerRegistry $doctrine): Response
-        {
+    {
 
-       // Récupération du cheval correspondant à l'identifiant
-             $cheval = $doctrine->getRepository(Cheval::class)->find(intval($idCheval));
-         // Vérification si le cheval existe
+        // Récupération du cheval correspondant à l'identifiant
+        $cheval = $doctrine->getRepository(Cheval::class)->find(intval($idCheval));
+        // Vérification si le cheval existe
 
-         
-        
+
         // Renvoi de la réponse avec les données du cheval
-             return $this->render('cheval/consulter.html.twig', [
-                 'cheval' => $cheval,
-    ]);
+        return $this->render('cheval/consulter.html.twig', [
+            'cheval' => $cheval,
+        ]);
 
     }
 
 
-
-
-    #[Route('api/cheval/consulter/{idCheval}', name:'app_cheval_consulter_api')]
+    #[Route('api/cheval/consulter/{idCheval}', name: 'app_cheval_consulter_api')]
     public function consulterChevalapi($idCheval, Request $request, ManagerRegistry $doctrine): Response
     {
 
@@ -83,23 +81,18 @@ class ChevalController extends AbstractController
         // Vérification si le cheval existe
 
 
-
         $encoders = [new XmlEncoder(), new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
 
         $serializer = new Serializer($normalizers, $encoders);
 
-        $jsonContent = $serializer->serialize($cheval, 'json',[AbstractItemNormalizer::IGNORED_ATTRIBUTES=>['categorieDeVentes','lots']]);
-        return new Response($jsonContent);
-    ;
+        $jsonContent = $serializer->serialize($cheval, 'json', [AbstractItemNormalizer::IGNORED_ATTRIBUTES => ['categorieDeVentes', 'lots']]);
+        return new Response($jsonContent);;
 
     }
 
 
-
-    
-    #[Route('/cheval/modifier/{id}', name:'app_cheval_modifier')]
-    
+    #[Route('/cheval/modifier/{id}', name: 'app_cheval_modifier')]
     public function modifier(Request $request, Cheval $cheval): Response
     {
         $form = $this->createForm(ChevalType::class, $cheval);
@@ -124,38 +117,47 @@ class ChevalController extends AbstractController
     {
         $entityManager = $doctrine->getManager();
         $chevals = $entityManager->getRepository(Cheval::class)->findAll();
-       /* $ventes = $this->getDoctrine()
-            ->getRepository(Vente::class)
-            ->findAll();*/
+        /* $ventes = $this->getDoctrine()
+             ->getRepository(Vente::class)
+             ->findAll();*/
 
         return $this->render('cheval/lister_chevals.html.twig', [
             'chevals' => $chevals,
         ]);
     }
+
     #[Route('api/cheval/lister', name: 'app_cheval_lister_api')]
     public function getLesChevalsapi(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
         $chevals = $entityManager->getRepository(Cheval::class)->findAll();
-        /* $ventes = $this->getDoctrine()
-             ->getRepository(Vente::class)
-             ->findAll();*/
 
-        $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
 
-        $serializer = new Serializer($normalizers, $encoders);
 
-        $jsonContent = $serializer->serialize($chevals, 'json',[AbstractItemNormalizer::IGNORED_ATTRIBUTES=>['categorieDeVentes','lots']]);
-        return new Response($jsonContent);
+
+        $jsonContent = [];
+        foreach ($chevals as $cheval) {
+            $nomCheval = $cheval->getNom();
+            $libelleraceDuCheval = $cheval->getRace()->getLibellle();
+            $prixDeVenteCheval = $cheval->getPrixDeDepart();
+            $listeCheval = ["nomCheval" => $nomCheval, "libelleraceDuCheval" => $libelleraceDuCheval, "prixDeVenteCheval" => $prixDeVenteCheval];
+            array_push($jsonContent, $listeCheval);
+
+
+        }
+
+        return new JsonResponse($jsonContent);;
 
     }
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
